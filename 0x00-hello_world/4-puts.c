5fc5e94225a346c3367cd3824391883a7e00cf63
EVC <vesionyeaba@gmail.com>
2023-02-22 07:29:06 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main A program that prints a line with puts function 
+ * main - function to display using puts
  * Return:0(success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
