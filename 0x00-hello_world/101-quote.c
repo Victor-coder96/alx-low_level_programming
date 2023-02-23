427bac15b6679e817ed161ceb72ebd61962b5599
EVC <vesionyeaba@gmail.com>
2023-02-23 03:28:32 -0800

    last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
 * main - A c program that prints a line to the standard error
 * Return:  1 (Success)
 */
 int main(void)
 {
-	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (1);
 }
