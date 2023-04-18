7d9e4d049c5aa26e7ea5cb0a3c20609e181ffd20
EVC <vesionyeaba@gmail.com>
2023-04-18 05:50:45 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t tnodes = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", t->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		nnodes++;
 	}
-	return (tnodes);
+	return (nnodes);
 }
+
