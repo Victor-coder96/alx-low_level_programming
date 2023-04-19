e018334816bccbd08ae31f8a3cba561b65be0b2e
EVC <vesionyeaba@gmail.com>
2023-04-19 16:19:11 -0700

    print bin num

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,12 @@
  *
  * Return: no return.
  */
-void print_binary_num (unsigned long int n)
+void print_binarynum (unsigned long int n)
 {
 	if (n >> 0)
 	{
 		if (n >> 1)
-			print_binary_num(n >> 1);
+			print_binarynum(n >> 1);
 		_putchar((n & 1) + '0');
 	}
 	else
