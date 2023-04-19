84f55cdd7fe66b770f91068299c44fd30c64dc5e
EVC <vesionyeaba@gmail.com>
2023-04-19 16:32:59 -0700

    task 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * to print_binary number - prints the binary representation
+ * to print_binary - prints the binary representation
  * of a number.
  * @n: unsigned long int.
  *
  * Return: no return.
  */
-void print_binarynum(unsigned long int n)
+void print_binary(unsigned long int n)
 {
 	if (n >> 0)
 	{
 		if (n >> 1)
-			print_binarynum(n >> 1);
+			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
 	}
 	else
